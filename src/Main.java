53037fd1843f594733a93c8db039946bee0c90fc
karanvir95 <karanvirsattwal@gmail.com>
2020-12-03 21:43:50 +0000

    change some text

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hi my name is Karanvir Attwal and this is my Docker App");
+        System.out.println("Hi my name is Karanvir Attwal and this is my Docker App and it will be deployed to minikube");
     }
 }
