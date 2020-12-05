a9b9c7062a4a88c54390823dd9fb7b9e406d7b4e
karanvir95 <karanvirsattwal@gmail.com>
2020-12-05 18:46:43 +0000

    Hello Harness new

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Harness, my name is Karanvir Attwal and this is my Docker App and it will be deployed to minikube");
+        System.out.println("Hello Harness, this is my Docker App and it will be deployed to minikube");
     }
 }
